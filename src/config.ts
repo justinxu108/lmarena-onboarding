b8911ba84fcd60d6b02712d8f44e1d2b82b4f375
Justin Xu <justinxu108@gmail.com>
2026-03-23 17:25:22 -0700

    Add Google Apps Script URL to config

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // Replace with your deployed Google Apps Script web app URL
-export const GOOGLE_SCRIPT_URL = '';
+export const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz4s8ogjOnwCTJz_8rGtze7FjNsO_NnvwiVgN0lFSHh8dp1ejGqqfWbU3sSxAERNkx-/exec';
